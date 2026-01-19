5734ac99c86a50a53fd9cbc226197c903abe8ff2
Masayuki Kawahima <poppinmasa@gmail.com>
2026-01-19 15:18:25 +0900

    session3完了

--- YumemiWeatherApp/YumemiWeatherApp/ViewController.swift
+++ YumemiWeatherApp/YumemiWeatherApp/ViewController.swift
@@ -37,7 +37,28 @@ class ViewController: UIViewController {
   }
   
   @IBAction func reloadButtonAction(_ sender: Any) {
+    setWeaterImageOfThorowsVer()
+  }
+  
+  // fetchWeatherCondition()のThrows verのメソッド
+  private func setWeaterImageOfThorowsVer() {
+    
+    do {
+      let result = try YumemiWeather.fetchWeatherCondition(at: "東京")
+      setImageWhenFetchWeatherConditionSucceeded(result: result)
+    } catch {
+      displayErrorAlert()
+    }
+  }
+  // fetchWeatherCondition()のsimple Verのメソッド
+  private func setWeaterImageOfSimpleVer() {
+    
     let result = YumemiWeather.fetchWeatherCondition()
+    setImageWhenFetchWeatherConditionSucceeded(result: result)
+  }
+  
+  // フェッチが成功した時に画像をセットするメソッド
+  private func setImageWhenFetchWeatherConditionSucceeded(result: String) {
     
     switch result {
     case "sunny":
@@ -49,10 +70,22 @@ class ViewController: UIViewController {
     default:
       break
     }
-    
-    
   }
   
+  // WeatherConditionのフェッチに失敗したときのアラートを表示するメソッド
+  private func displayErrorAlert() {
+    
+    let alert = UIAlertController(title: "天気情報を取得できませんでした", message: "再読み込みをしてください", preferredStyle: .alert)
+    let reloadAction = UIAlertAction(title: "再読み込み", style: .default) { _ in
+      alert.dismiss(animated: true, completion: nil)
+      self.setWeaterImageOfThorowsVer()
+    }
+    let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel)
+    
+    alert.addAction(reloadAction)
+    alert.addAction(cancelAction)
+    present(alert, animated: true)
+  }
   
 }
 
