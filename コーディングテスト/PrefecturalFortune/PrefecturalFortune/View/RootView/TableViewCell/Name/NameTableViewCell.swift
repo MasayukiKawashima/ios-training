963503331b43bc5a9eb308847cce0777134220ac
Masayuki Kawahima <poppinmasa@gmail.com>
2026-02-22 13:50:29 +0900

    NameTableViewCellの仮レイアウト作成

---
+++ コーディングテスト/PrefecturalFortune/PrefecturalFortune/View/RootView/TableViewCell/Name/NameTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NameTableViewCell.swift
+//  PrefecturalFortune
+//
+//  Created by 川島真之 on 2026/02/22.
+//
+
+import UIKit
+
+class NameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
