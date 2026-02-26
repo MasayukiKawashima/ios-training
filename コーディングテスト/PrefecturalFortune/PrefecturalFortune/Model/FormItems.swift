346d8740fb7cfde837bd34289c112c41fd6ea2de
Masayuki Kawahima <poppinmasa@gmail.com>
2026-02-26 19:43:44 +0900

    feat: FormFieldの機能拡張
    FormFieldの各プロパティがnilでないかをチェックするメソッドとそれに必要なenumの追加

--- コーディングテスト/PrefecturalFortune/PrefecturalFortune/Model/FormItems.swift
+++ コーディングテスト/PrefecturalFortune/PrefecturalFortune/Model/FormItems.swift
@@ -7,9 +7,37 @@
 
 import Foundation
 
+enum FormField {
+    case name
+    case dateOfBirth
+    case bloodType
+}
+
 struct FormItems {
 
   var name: String?
   var dateOfBirth: Date?
   var bloodType: BloodType?
+}
+
+extension FormItems {
+
+  func missingFields() -> [FormField] {
+
+    var fields: [FormField] = []
+    
+    if name == nil {
+      fields.append(.name)
+    }
+
+    if dateOfBirth == nil {
+      fields.append(.dateOfBirth)
+    }
+
+    if bloodType == nil {
+      fields.append(.bloodType)
+    }
+
+    return fields
+  }
 }
